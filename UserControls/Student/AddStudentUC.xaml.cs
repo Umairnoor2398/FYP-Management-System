a0d9c826810d4655d201123adb5e2ceefaf507a9
Umair Noor <unoor2398@gmail.com>
2023-02-21 22:08:45 +0500

    Student CRUD Operations Completed

--- UserControls/Student/AddStudentUC.xaml.cs
+++ UserControls/Student/AddStudentUC.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CRUD_Operations;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +22,26 @@ namespace ProjectA.UserControls.Student
     /// </summary>
     public partial class AddStudentUC : UserControl
     {
+        int gender = 1;
+        int id;
         public AddStudentUC()
         {
             InitializeComponent();
+            addBtn.Content = "Add";
         }
 
-        public void genderBtnClicked(object sender, RoutedEventArgs e)
+        public AddStudentUC(string FName, string LName, string contact, string email, int gender, string regno, string dob, int id)
         {
-            if (sender.Equals(maleBtn))
+            InitializeComponent();
+            addBtn.Content = "Update";
+            txtFirstName.Text = FName;
+            txtLastName.Text = LName;
+            txtContact.Text = contact;
+            txtEmail.Text = email;
+            txtRegNo.Text = regno;
+            dobDatePicker.SelectedDate = DateTime.Parse(dob);
+            this.id = id;
+            if (gender == 1)
             {
                 maleBtn.Background = Brushes.Purple;
                 femaleBtn.Background = Brushes.MediumPurple;
@@ -37,6 +51,90 @@ namespace ProjectA.UserControls.Student
                 maleBtn.Background = Brushes.MediumPurple;
                 femaleBtn.Background = Brushes.Purple;
             }
+        }
+
+        public void genderBtnClicked(object sender, RoutedEventArgs e)
+        {
+            if (sender.Equals(maleBtn))
+            {
+                maleBtn.Background = Brushes.Purple;
+                femaleBtn.Background = Brushes.MediumPurple;
+                gender = 1;
+            }
+            else
+            {
+                maleBtn.Background = Brushes.MediumPurple;
+                femaleBtn.Background = Brushes.Purple;
+                gender = 2;
+            }
+        }
+
+        private void revertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            txtFirstName.Clear();
+            txtRegNo.Clear();
+            txtEmail.Clear();
+            txtLastName.Clear();
+            txtContact.Clear();
+            maleBtn.Background = Brushes.Purple;
+            femaleBtn.Background = Brushes.MediumPurple;
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (addBtn.Content.ToString() == "Add")
+            {
+
+                try
+                {
+                    var con = Configuration.getInstance().getConnection();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Person(FirstName,LastName,Contact,Email,DateOfBirth,Gender) VALUES (@FirstName,@LastName, @Contact,@Email,@DateOfBirth, @Gender)", con);
+                    SqlCommand cmd1 = new SqlCommand("insert into Student(Id,RegistrationNo) VALUES ((SELECT Id FROM Person WHERE FirstName = @FirstName AND LastName=@LastName AND Contact=@Contact AND Email=@Email AND DateOfBirth=@DateOfBirth AND Gender=@Gender) ,@RegNo)", con);
+                    cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                    cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                    cmd.Parameters.AddWithValue("@Contact", txtContact.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@DateOfBirth", dobDatePicker.SelectedDate.ToString());
+                    cmd.Parameters.AddWithValue("@Gender", gender);
+                    cmd1.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                    cmd1.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                    cmd1.Parameters.AddWithValue("@Contact", txtContact.Text);
+                    cmd1.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    cmd1.Parameters.AddWithValue("@DateOfBirth", dobDatePicker.SelectedDate.ToString());
+                    cmd1.Parameters.AddWithValue("@Gender", gender);
+                    cmd1.Parameters.AddWithValue("@RegNo", txtRegNo.Text);
+                    cmd.ExecuteNonQuery();
+                    cmd1.ExecuteNonQuery();
+                    MessageBox.Show("Successfully saved");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    var con = Configuration.getInstance().getConnection();
+                    SqlCommand cmd = new SqlCommand("UPDATE Person SET FirstName = @FirstName, LastName=@LastName, Contact=@Contact, Email=@Email, DateOfBirth=@DateOfBirth, Gender=@Gender WHERE Id=@Id; UPDATE Student SET RegistrationNo=@RegNo WHERE Id=@Id;", con);
+                    cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                    cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                    cmd.Parameters.AddWithValue("@Contact", txtContact.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@DateOfBirth", dobDatePicker.SelectedDate.ToString());
+                    cmd.Parameters.AddWithValue("@Gender", gender);
+                    cmd.Parameters.AddWithValue("@RegNo", txtRegNo.Text);
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Record Updated");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
         }
     }
 }
--- UserControls/Student/CRUDStudent.xaml.cs
+++ UserControls/Student/CRUDStudent.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CRUD_Operations;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +27,87 @@ namespace ProjectA.UserControls
         {
             InitializeComponent();
             studentCC.Content = new UserControls.Student.AddStudentUC();
+            ViewStudent();
+        }
+
+        private void ViewStudent()
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("Select * from Person P JOIN Student S ON S.Id=P.Id", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            StudentDataGrid.ItemsSource = dt.DefaultView;
+        }
+
+        private void editBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(StudentDataGrid.SelectedIndex.ToString());
+            string FName, LName, contact, email, regno, dob;
+            int gender;
+            DataRowView row = StudentDataGrid.SelectedItem as DataRowView;
+            if (row != null)
+            {
+                int id = Int32.Parse(row["Id"].ToString());
+                FName = row["FirstName"].ToString();
+                LName = row["LastName"].ToString();
+                contact = row["Contact"].ToString();
+                email = row["Email"].ToString();
+                regno = row["RegistrationNo"].ToString();
+                dob = row["DateOfBirth"].ToString();
+                gender = int.Parse(row["Gender"].ToString());
+                studentCC.Content = new Student.AddStudentUC(FName, LName, contact, email, gender, regno, dob, id);
+                addStudentForm.Visibility = Visibility.Visible;
+                viewStudent.Visibility = Visibility.Collapsed;
+                addStBtn.Content = "Back";
+            }
+
+        }
+        private void deleteRecord(int id)
+        {
+            try
+            {
+                var con = Configuration.getInstance().getConnection();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Student Where Id=@Id; DELETE FROM Person Where Id=@Id;", con);
+                //SqlCommand cmd1 = new SqlCommand("DELETE FROM Person Where Id=@Id", con);
+                cmd.Parameters.AddWithValue("@Id", id);
+                //cmd1.Parameters.AddWithValue("@Id", id);
+                cmd.ExecuteNonQuery();
+                //cmd1.ExecuteNonQuery();
+                MessageBox.Show("Record Deleted!!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView row = StudentDataGrid.SelectedItem as DataRowView;
+            if (row != null)
+            {
+                int id = Int32.Parse(row["Id"].ToString());
+                deleteRecord(id);
+                ViewStudent();
+            }
+        }
+
+        private void addStBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (addStBtn.Content.ToString() == "Add")
+            {
+                addStudentForm.Visibility = Visibility.Visible;
+                viewStudent.Visibility = Visibility.Collapsed;
+                addStBtn.Content = "Back";
+            }
+            else
+            {
+                ViewStudent();
+                addStudentForm.Visibility = Visibility.Collapsed;
+                viewStudent.Visibility = Visibility.Visible;
+                addStBtn.Content = "Add";
+            }
         }
     }
 }
